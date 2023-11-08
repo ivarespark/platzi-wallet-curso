2cc4d1d01e36bc33d468d447f882f58e21eea7d9
Ivan Espinosa <ivarespar@gmail.com>
2023-11-07 22:44:21 -0500

    [patt] Patŕon Factory

--- app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/HomeFragment.kt
+++ app/src/main/java/com/cristianvillamil/platziwallet/ui/home/view/HomeFragment.kt
@@ -11,6 +11,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.cristianvillamil.platziwallet.R
 import com.cristianvillamil.platziwallet.ui.home.FavoriteTransfer
 import com.cristianvillamil.platziwallet.ui.home.HomeContract
+import com.cristianvillamil.platziwallet.ui.home.data.MessageFactory
+import com.cristianvillamil.platziwallet.ui.home.data.MessageFactory.Companion.TYPE_ERROR
 import com.cristianvillamil.platziwallet.ui.home.presenter.HomePresenter
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.fragment_home.*
@@ -62,5 +64,12 @@ class HomeFragment : Fragment(), HomeContract.View {
 
     override fun showFavoriteTransfers(favoriteTransfer: List<FavoriteTransfer>) {
         favoriteTransferAdapter.setData(favoriteTransfer)
+        val dialogFactory : MessageFactory = MessageFactory()
+        context?.let{
+            // Si contexto no es nulo ejecuta la linea
+            val errorDialog = dialogFactory.getDialog(it, TYPE_ERROR)
+            errorDialog.show()
+        }
+
     }
 }
